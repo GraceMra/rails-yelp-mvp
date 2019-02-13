e654072889b69d174435bc7edd6f772abb89c98a
Grace Mia <grace.mehrabe@gmail.com>
2019-02-13 12:32:09 +0100

    all set, style missing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,15 +8,19 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
-    # we need `restaurant_id` to asssociate review with corresponding restaurant
-    @review.restaurant = Restaurant.find(params[:restaurant_id])
-    @review.save
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
-
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+bristol = Restaurant.create(name: "Epicure", category: "french", address: "112 Rue du Faubourg Saint-Honoré, 75008 Paris", phone_number: "01 53 43 43 40")
+le_cinq = Restaurant.create(name: "Le Cinq", category: "french", address: "31 Avenue George V, 75008 Paris", phone_number: "01 53 43 43 42")
+kei = Restaurant.create(name: "Restaurant Kei", category: "japanese", address: "5 Rue Coq Héron, 75001 Paris")
+pink_mamma = Restaurant.create(name: "Pink Mamma", category: "italian", address: "20bis Rue de Douai, 75009 Paris", phone_number: "01 53 43 43 45")
+chez_justine = Restaurant.create(name: "Chez Justine", category: "french", address: "96 Rue Oberkampf, 75011 Paris", phone_number: "01 53 43 43 47")
